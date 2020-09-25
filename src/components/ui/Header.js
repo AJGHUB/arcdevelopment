d5435a7ead7b9d7a7057e7b5c6a34d8858264597
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-25 15:40:57 +1200

    nav RRDom

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
-import { Button } from '@material-ui/core';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -49,6 +50,27 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header(props) {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 0) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 0) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 0) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 0) {
+      setValue(4);
+    } else if (window.location.pathname === '/estimate' && value !== 0) {
+      setValue(5);
+    }
+  }, [value]); //constant in array tells useeffect hook that state value is dependant not to run code infinite
 
   return (
     <React.Fragment>
@@ -56,12 +78,42 @@ export default function Header(props) {
         <AppBar position="fixed">
           <Toolbar disableGutters>
             <img alt="company logo" src={logo} />
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="The Revolution" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor="primary"
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
             </Tabs>
             <Button
               variant="contained"
