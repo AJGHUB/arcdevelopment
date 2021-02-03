908772e6608b8616538cadb1bc9ff70c70f7604d
Alice Jowsey <12345alcatraz@gmail.com>
2021-02-03 20:56:33 +1300

    footer icons

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -7,6 +7,13 @@ import Hidden from '@material-ui/core/Hidden'
 
 
 import footerAdornment from '../../assets/Footer Adornment.svg';
+import facebook from "../../assets/facebook.svg"
+import twitter from "../../assets/twitter.svg"
+import instagram from "../../assets/instagram.svg"
+
+
+
+
 
 const useStyles = makeStyles(theme => ({
     footer: {
@@ -37,9 +44,25 @@ const useStyles = makeStyles(theme => ({
     },
     gridItem: {
         margin: "3em"
+    },
+    icon: {
+        height: "4em",
+        width: "4em",
+        [theme.breakpoints.down("xs")]: {
+            height: "2.5em",
+            width: "2.5em"
+        }
+
+    },
+    socialContainer: {
+        position: "absolute",
+        marginTop: "-6m",
+        // backgroundColor: theme.palette.common.blue, //Adding this I can at least see them.
+        right: "1.5em",
+        [theme.breakpoints.down("xs")]: {
+            right: "0.6em",
+        }
     }
-
-
 }))
 
 
@@ -50,8 +73,13 @@ export default function Footer(props) {
     return (
         <footer className={classes.footer}>
         <Hidden mdDown> 
-            <Grid container justify="center" className={classes.mainContainer}>
-                <Grid item className={classes.gridItem}>
+            <Grid 
+            container 
+            justify="flex-end"
+            className={classes.mainContainer}>
+                <Grid 
+                    item 
+                    className={classes.gridItem}>
                     <Grid container direction="column" spacing={2}>
                         <Grid 
                         item
@@ -64,7 +92,9 @@ export default function Footer(props) {
                         </Grid>
                     </Grid>             
                 </Grid>
-                <Grid item className={classes.gridItem}>
+                <Grid 
+                    item 
+                    className={classes.gridItem}>
                     <Grid container direction="column" spacing={2}>
                         <Grid 
                             item 
@@ -104,7 +134,9 @@ export default function Footer(props) {
                         </Grid>
                     </Grid>             
                 </Grid>
-                <Grid item className={classes.gridItem}>
+                <Grid 
+                    item 
+                    className={classes.gridItem}>
                     <Grid container direction="column" spacing={2}>
                         <Grid 
                             item 
@@ -144,7 +176,9 @@ export default function Footer(props) {
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item className={classes.gridItem}>
+                <Grid
+                     item 
+                     className={classes.gridItem}>
                     <Grid container direction="column" spacing={2} >
                        <Grid 
                        item 
@@ -174,7 +208,9 @@ export default function Footer(props) {
                        </Grid>
                     </Grid>              
                 </Grid>
-                <Grid item className={classes.gridItem}>
+                <Grid 
+                    item 
+                    className={classes.gridItem}>
                     <Grid container direction="column" spacing={2}>
                     <Grid
                      item 
@@ -189,10 +225,46 @@ export default function Footer(props) {
            </Grid>
         </Hidden>
         <img 
-        alt="black decorative slash" 
-        src={footerAdornment} 
-        className={classes.adornment}
+            alt="black decorative slash" 
+            src={footerAdornment} 
+            className={classes.adornment}
          />
+
+         <Grid 
+            container 
+            justify="flex-end" 
+            spacing={2} 
+            className={classes.socialContainer}
+            >
+           <Grid 
+                item 
+                component={"a"} 
+                href="https://www.facebook.com" 
+                rel="noopener noreferrer" 
+                target="_blank"
+                >
+                <img alt="facebook logo" src={facebook} className={classes.icon} />
+           </Grid>  
+           <Grid 
+                item 
+                component={"a"} 
+                href="https://www.twitter.com" 
+                rel="noopener noreferrer" 
+                target="_blank"
+                >
+              <img alt="twitter logo" src={twitter} className={classes.icon} />
+           </Grid> 
+           <Grid 
+                item 
+                component={"a"} 
+                href="https://www.instagram.com" 
+                rel="noopener noreferrer" 
+                target="_blank"
+                >
+              <img alt="instagram logo" src={instagram} className={classes.icon} />
+           </Grid> 
+         </Grid>
+         
         </footer>
     );
 }
